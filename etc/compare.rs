361d0fdbea49ed3c12150023a43da822df4cc6fc
León Orell Valerian Liehr <me@fmease.dev>
2025-05-30 17:15:33 +0200

    Support edition in the compare script

--- etc/compare.rs
+++ etc/compare.rs
@@ -14,6 +14,7 @@ compile_error!("non-Unix platforms not supported");
 
 use std::{
     env,
+    ffi::OsString,
     path::{Path, PathBuf},
     process::{Command, ExitCode, ExitStatus, Stdio},
     time::Instant,
@@ -29,14 +30,19 @@ fn main() -> ExitCode {
 fn try_main() -> Result<(), ()> {
     let mut args = env::args_os().skip(1);
     let mut path = None;
-    let mut skip_true_ill = false;
-    let mut invert = false;
+    let mut opts = Opts::default();
 
     while let Some(arg) = args.next() {
         if let Some(opt) = arg.as_encoded_bytes().strip_prefix(b"--") {
             match opt {
-                b"skip-true-ill" => skip_true_ill = true,
-                b"invert" => invert = true,
+                b"skip-true-ill" => opts.skip_true_ill = true,
+                b"invert" => opts.invert = true,
+                b"edition" => {
+                    // FIXME: Deny passing that args mult times?
+                    opts.edition = Some(args.next().ok_or_else(|| {
+                        eprintln!("error: missing required argument `<EDITION>`")
+                    })?);
+                }
                 _ => {
                     eprintln!("error: unknown flag `{}`", arg.display());
                     return Err(());
@@ -51,7 +57,7 @@ fn try_main() -> Result<(), ()> {
         }
     }
 
-    let path = path.ok_or_else(|| eprintln!("error: missing required path argument"))?;
+    let path = path.ok_or_else(|| eprintln!("error: missing required argument `<PATH>`"))?;
 
     let rasur_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../target/release/rasur"))
         .with_extension(env::consts::EXE_EXTENSION);
@@ -79,7 +85,7 @@ fn try_main() -> Result<(), ()> {
             && let Some(ext) = path.extension()
             && ext == "rs"
         {
-            let result = compare(path, &rasur_path, &rustc_path, skip_true_ill, invert);
+            let result = compare(path, &rasur_path, &rustc_path, &opts);
             if let Some(result) = result {
                 total += 1;
 
@@ -90,7 +96,7 @@ fn try_main() -> Result<(), ()> {
                     let Some(rasur_exit_code) = rasur_exit_status.code() else { continue };
                     let Some(rustc_exit_code) = rustc_exit_status.code() else { continue };
 
-                    let op = if invert { "==" } else { "/=" };
+                    let op = if opts.invert { "==" } else { "/=" };
 
                     println!(
                         "{}: rasur({rasur_exit_code}) {op} rustc({rustc_exit_code})",
@@ -119,33 +125,45 @@ fn try_main() -> Result<(), ()> {
     Ok(())
 }
 
+#[derive(Default)]
+struct Opts {
+    skip_true_ill: bool,
+    invert: bool,
+    edition: Option<OsString>,
+}
+
 fn compare(
     path: &Path,
     rasur_path: &Path,
     rustc_path: &Path,
-    skip_true_ill: bool,
-    invert: bool,
+    opts: &Opts,
 ) -> Option<Result<(), (ExitStatus, ExitStatus)>> {
-    let rustc_exit_status = Command::new(rustc_path)
+    let mut rustc_call = Command::new(rustc_path);
+    rustc_call
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .arg(&path)
-        .arg("-Zparse-crate-root-only")
-        .status()
-        .expect("failed to execute `rustc`");
+        .arg("-Zparse-crate-root-only");
+    if let Some(edition) = &opts.edition {
+        rustc_call.arg("--edition");
+        rustc_call.arg(edition);
+    }
 
-    if skip_true_ill && !rustc_exit_status.success() {
+    let rustc_exit_status = rustc_call.status().expect("failed to execute `rustc`");
+
+    if opts.skip_true_ill && !rustc_exit_status.success() {
         return None;
     }
 
-    let rasur_exit_status = Command::new(rasur_path)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .arg(&path)
-        .status()
-        .expect("failed to execute `rasur`");
+    let mut rasur_call = Command::new(rasur_path);
+    rasur_call.stdout(Stdio::null()).stderr(Stdio::null()).arg(&path);
+    if let Some(edition) = &opts.edition {
+        rasur_call.arg("--edition");
+        rasur_call.arg(edition);
+    }
+    let rasur_exit_status = rasur_call.status().expect("failed to execute `rasur`");
 
-    if (rasur_exit_status == rustc_exit_status) == invert {
+    if (rasur_exit_status == rustc_exit_status) == opts.invert {
         return Some(Err((rasur_exit_status, rustc_exit_status)));
     }
 
