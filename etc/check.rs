f3b3d8af795334183d2be008b6f4c5278c8a313c
León Orell Valerian Liehr <me@fmease.dev>
2025-12-07 19:39:55 +0100

    Parallelize & otherwise improve check script

--- etc/check.rs
+++ etc/check.rs
@@ -14,10 +14,14 @@ compile_error!("non-Unix platforms not supported");
 use std::{
     env,
     ffi::OsString,
+    num::NonZeroUsize,
     path::{Path, PathBuf},
     process::{Command, ExitCode, ExitStatus, Stdio},
+    sync::{Arc, Mutex},
     time::Instant,
 };
+
+const DEFAULT_CHUNK_SIZE: NonZeroUsize = NonZeroUsize::new(10).unwrap(); // idk
 
 fn main() -> ExitCode {
     match try_main() {
@@ -28,36 +32,57 @@ fn main() -> ExitCode {
 
 fn try_main() -> Result<(), ()> {
     let mut args = env::args_os().skip(1);
-    let mut path = None;
+
     let mut opts = Opts::default();
+    let mut paths = Vec::new();
+    let mut jobs = None::<NonZeroUsize>;
+    let mut chunk_size = None::<NonZeroUsize>;
+
+    let mut parse_opts = true;
 
     while let Some(arg) = args.next() {
-        if let Some(opt) = arg.as_encoded_bytes().strip_prefix(b"--") {
+        if parse_opts && let Some(opt) = arg.as_encoded_bytes().strip_prefix(b"-") {
             match opt {
-                b"skip-true-ill" => opts.skip_true_ill = true,
-                b"invert" => opts.invert = true,
-                b"edition" => {
-                    // FIXME: Deny passing that args mult times?
-                    opts.edition = Some(args.next().ok_or_else(|| {
-                        eprintln!("error: missing required argument `<EDITION>`")
-                    })?);
+                b"-" => parse_opts = false,
+                b"c" | b"-chunk" => {
+                    chunk_size = Some(
+                        args.next()
+                            .ok_or_else(|| eprintln!("error: missing required argument `SIZE`"))?
+                            .to_str()
+                            .and_then(|chunk| chunk.parse().ok())
+                            .ok_or_else(|| eprintln!("error: argument `SIZE` is invalid"))?,
+                    )
                 }
-                b"verbose" => opts.verbose = true,
+                b"e" | b"-edition" => {
+                    opts.edition =
+                        Some(args.next().ok_or_else(|| {
+                            eprintln!("error: missing required argument `EDITION`")
+                        })?);
+                }
+                b"j" | b"-jobs" => {
+                    jobs = Some(
+                        args.next()
+                            .ok_or_else(|| {
+                                eprintln!("error: missing required argument `JOBS`");
+                            })?
+                            .to_str()
+                            .and_then(|jobs| jobs.parse().ok())
+                            .ok_or_else(|| {
+                                eprintln!("error: argument `JOBS` is invalid");
+                            })?,
+                    );
+                }
+                b"-skip-true-ill" => opts.skip_true_ill = true,
+                b"v" | b"-verbose" => opts.verbose = true,
                 _ => {
                     eprintln!("error: unknown flag `{}`", arg.display());
                     return Err(());
                 }
             }
         } else {
-            if path.is_some() {
-                eprintln!("error: unexpected argument `{}`", arg.display());
-                return Err(());
-            }
-            path = Some(PathBuf::from(arg));
+            paths.push(PathBuf::from(arg));
         }
     }
-
-    let path = path.ok_or_else(|| eprintln!("error: missing required argument `<PATH>`"))?;
 
     let rasur_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../target/release/rasur"))
         .with_extension(env::consts::EXE_EXTENSION);
@@ -68,66 +93,63 @@ fn try_main() -> Result<(), ()> {
         PathBuf::from(String::from_utf8(output.stdout).unwrap())
     };
 
-    let mut total = 0usize;
-    let mut failures = 0usize;
+    let jobs = jobs.unwrap_or_else(|| std::thread::available_parallelism().unwrap());
+    let chunk_size = chunk_size.unwrap_or(DEFAULT_CHUNK_SIZE);
 
-    // FIXME: Parallelize
-    // FIXME: Don't eagerly report to stderr (slows things down)
-    // FIXME: Check all editions 2015..2024 per file!
+    let entries = paths.into_iter().flat_map(walkdir::WalkDir::new);
+    let entries = Arc::new(Mutex::new(entries));
 
     let time = Instant::now();
 
-    for entry in walkdir::WalkDir::new(path) {
-        let entry = entry.expect("failed to read dir entry");
+    let stats = std::thread::scope(|scope| {
+        let handles: Vec<_> = (0..jobs.get())
+            .map(|_| {
+                let entries = entries.clone();
+                let rasur_path = &rasur_path;
+                let rustc_path = &rustc_path;
+                let opts = &opts;
 
-        if entry.file_type().is_file()
-            && let path = entry.path()
-            && let Some(ext) = path.extension()
-            && ext == "rs"
-        {
-            if opts.verbose {
-                print!("{}: ", path.display());
-                use std::io::Write as _;
-                std::io::stdout().flush().unwrap();
-            }
-            let result = compare(path, &rasur_path, &rustc_path, &opts);
-            if let Some(result) = result {
-                total += 1;
+                scope.spawn(move || {
+                    let mut local_entries = Vec::with_capacity(chunk_size.get());
+                    let mut stats = Stats::default();
 
-                if let Err((rasur_exit_status, rustc_exit_status)) = result {
-                    failures += 1;
+                    loop {
+                        {
+                            let mut entries = entries.lock().unwrap();
 
-                    // FIXME: Log non-exit codes (signals, …)
-                    let Some(rasur_exit_code) = rasur_exit_status.code() else { continue };
-                    let Some(rustc_exit_code) = rustc_exit_status.code() else { continue };
+                            for _ in 0..chunk_size.get() {
+                                local_entries.push(match entries.next() {
+                                    Some(entry) => entry,
+                                    None => break,
+                                });
+                            }
+                        }
 
-                    let op = if opts.invert { "==" } else { "/=" };
+                        if local_entries.is_empty() {
+                            break;
+                        }
 
-                    if !opts.verbose {
-                        print!("{}: ", path.display());
+                        for entry in local_entries.drain(..) {
+                            check(entry, rasur_path, rustc_path, opts, &mut stats);
+                        }
                     }
-                    println!("rasur({rasur_exit_code}) {op} rustc({rustc_exit_code})");
-                } else if opts.verbose {
-                    println!("OK");
-                }
-            } else if opts.verbose {
-                println!("SKIPPED");
-            }
-        }
-    }
 
-    let status = if failures > 0 { "FAIL" } else { "OK" };
-    let successes = total - failures;
-    let percentage = if total == 0 { 1. } else { (successes as f32 / total as f32) * 100. };
+                    stats
+                })
+            })
+            .collect();
 
-    if failures > 0 {
-        // Visual separator
-        println!();
-    }
+        handles
+            .into_iter()
+            .fold(Stats::default(), |stats, handle| stats.adjoin(handle.join().unwrap()))
+    });
 
-    println!("[{status}] {successes}/{total} ({percentage:.2}%) | {:?}", time.elapsed());
+    let duration = time.elapsed();
 
-    if failures > 0 {
+    stats.render(&opts);
+    println!("    {duration:?}");
+
+    if !stats.failed.is_empty() {
         return Err(());
     }
 
@@ -137,9 +159,90 @@ fn try_main() -> Result<(), ()> {
 #[derive(Default)]
 struct Opts {
     skip_true_ill: bool,
-    invert: bool,
     edition: Option<OsString>,
     verbose: bool,
+}
+
+#[derive(Default)]
+struct Stats {
+    failed: Vec<(PathBuf, ExitStatus, ExitStatus)>,
+    total: usize,
+    ignored: usize,
+}
+
+impl Stats {
+    fn adjoin(mut self, mut other: Self) -> Self {
+        self.failed.append(&mut other.failed);
+        self.total += other.total;
+        self.ignored += other.ignored;
+        self
+    }
+
+    fn render(&self, opts: &Opts) {
+        let Self { ref failed, total, ignored } = *self;
+
+        if !failed.is_empty() {
+            println!("=== FAILURES ===");
+            for &(ref path, rasur_exit_status, rustc_exit_status) in failed {
+                if opts.verbose {
+                    // FIXME: Handle non-codes (signals, ..)
+                    let print = |status: ExitStatus| {
+                        if let Some(code) = status.code() {
+                            print!("{code}");
+                        } else {
+                            print!("?");
+                        }
+                    };
+
+                    print(rasur_exit_status);
+                    print!("v");
+                    print(rustc_exit_status);
+                    print!("  ");
+                }
+
+                println!("{}", path.display());
+            }
+            println!("================");
+            println!();
+        }
+
+        println!("{}", if failed.is_empty() { "ALL TESTS PASSED!" } else { "SOME TESTS FAILED!" });
+
+        let failed = failed.len();
+        let passed = total - failed;
+        let percentage = if total == 0 { 1. } else { (passed as f32 / total as f32) * 100. };
+
+        println!(
+            "    {passed} passed ({percentage:.2}%) | {failed} failed | {ignored} ignored | {total} in total"
+        );
+    }
+}
+
+fn check(
+    entry: Result<walkdir::DirEntry, walkdir::Error>,
+    rasur_path: &Path,
+    rustc_path: &Path,
+    opts: &Opts,
+    stats: &mut Stats,
+) {
+    let entry = entry.expect("failed to read dir entry");
+
+    if entry.file_type().is_file()
+        && let path = entry.path()
+        && let Some(ext) = path.extension()
+        && ext == "rs"
+    {
+        let result = compare(path, rasur_path, rustc_path, opts);
+        if let Some(result) = result {
+            stats.total += 1;
+
+            if let Err((rasur_exit_status, rustc_exit_status)) = result {
+                stats.failed.push((entry.into_path(), rasur_exit_status, rustc_exit_status));
+            }
+        } else {
+            stats.ignored += 1;
+        }
+    }
 }
 
 fn compare(
@@ -173,7 +276,7 @@ fn compare(
     }
     let rasur_exit_status = rasur_call.status().expect("failed to execute `rasur`");
 
-    if (rasur_exit_status == rustc_exit_status) == opts.invert {
+    if rasur_exit_status != rustc_exit_status {
         return Some(Err((rasur_exit_status, rustc_exit_status)));
     }
 
